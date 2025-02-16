8d22670b67eb5052bdece1587cdbcb0c11f57f42
FxRayHughes <bushuaichen@yeah.net>
2025-02-16 19:17:55 +0800

    + 新增文件夹读取器

--- src/main/kotlin/top/maplex/arim/Arim.kt
+++ src/main/kotlin/top/maplex/arim/Arim.kt
@@ -14,35 +14,18 @@ object Arim {
 
     /**
      * 条件表达式求值器
-     * evaluator.evaluate("1 > 2") => false
-     * evaluator.evaluate("a < 2",a to 5) => false
-     *
-     * 变量值会自动转换为字符串类型
-     * evaluator.evaluate("status == 'ok'", "status" to "ok") // true
-     *
-     * 支持括号
-     * evaluator.evaluate("(5 > 3 || 2 < 1) && 'a' != 'b'") // true
-     *
-     * 必须使用空格分隔元素
-     * ✅ 正确：var > 5 && var < 10
-     * ❌ 错误：var>5&&var<10
-     *
-     * 字符串必须加引号
-     * ✅ 正确：name == 'John'
-     * ❌ 错误：name == John
      */
     val evaluator by lazy { ConditionEvaluator() }
 
     /**
      *  固定算数表达式计算器
-     *  相比之下，这个计算器更适合计算一些固定公式
-     *  如果自己处理变量的解析则不如变量算数表达式计算器
-     *
-     *  fixedCalculator.evaluate("1 + 2 * 3") => 7
      */
     val fixedCalculator by lazy { FixedCalculator() }
 
 
+    /**
+     *  变量计算器
+     */
     val variableCalculator by lazy { VariableCalculator() }
 
 
