38031c9723acf58f38f43f864c1fb6e28e6e4731
FxRayHughes <bushuaichen@yeah.net>
2025-02-17 19:57:36 +0800

    + 更新版本号

--- src/main/kotlin/top/maplex/arim/tools/entitymatch/EntityMatch.kt
+++ src/main/kotlin/top/maplex/arim/tools/entitymatch/EntityMatch.kt
@@ -1,11 +1,11 @@
 package top.maplex.arim.tools.entitymatch
 
-import org.bukkit.entity.Entity
 import org.bukkit.entity.LivingEntity
 import taboolib.module.chat.colored
 import top.maplex.arim.tools.entitymatch.handler.*
 import top.maplex.arim.tools.entitymatch.util.ParserUtils
 import java.util.concurrent.ConcurrentHashMap
+
 /**
  * 实体匹配公式
  * @Author WhiteSoul
@@ -29,18 +29,19 @@ class EntityMatch {
         registerHandler("meta", MetaHandler())
         registerHandler("health", HealthHandler())
     }
-        fun match(entity: LivingEntity, match: String): Boolean {
-            return ParserUtils.splitConditions(match).all { condition ->
-                val (key, value) = ParserUtils.parseKeyValue(condition) ?: return@all false
-                handlers[key]?.check(entity, value.colored()) ?: false
-            }
-        }
 
-        fun matchTry(entity: LivingEntity, match: String): Boolean {
-            return try {
-                match(entity, match)
-            } catch (e: Exception) {
-                false
-            }
+    fun match(entity: LivingEntity, match: String): Boolean {
+        return ParserUtils.splitConditions(match).all { condition ->
+            val (key, value) = ParserUtils.parseKeyValue(condition) ?: return@all false
+            handlers[key]?.check(entity, value.colored()) ?: false
+        }
+    }
+
+    fun matchTry(entity: LivingEntity, match: String): Boolean {
+        return try {
+            match(entity, match)
+        } catch (e: Exception) {
+            false
+        }
     }
 }
