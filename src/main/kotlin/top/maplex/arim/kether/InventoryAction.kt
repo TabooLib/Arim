abcee7687c87a9fab1c6c4a4a3c8748e3d965912
嘿鹰 <hhhhhy1747@outlook.com>
2025-02-25 15:17:57 +0800

    feat(kether): 添加物品数量检查和获取功能
    - 增加了 "take" 类型，用于从玩家背包中移除指定物品
    - 增加了 "count" 类型，用于统计玩家背包中匹配指定条件的物品数量
    - 优化了 "check" 和 "has" 类型的实现，提高了物品匹配的准确性

--- src/main/kotlin/top/maplex/arim/kether/InventoryAction.kt
+++ src/main/kotlin/top/maplex/arim/kether/InventoryAction.kt
@@ -4,12 +4,15 @@ import org.bukkit.entity.Player
 import taboolib.module.kether.KetherParser
 import taboolib.module.kether.combinationParser
 import taboolib.module.kether.player
+import taboolib.platform.util.countItem
 import taboolib.platform.util.hasItem
+import taboolib.platform.util.takeItem
 import top.maplex.arim.Arim
 import kotlin.jvm.optionals.getOrElse
 
 /**
  * 用法
+ * https://taboolib.feishu.cn/wiki/SRg4wO9q0iI3kbkdX79cHQRWncb
  *
  * arim-inv / arim-inventory
  *
@@ -26,9 +29,26 @@ fun parseInventory() = combinationParser {
     ).apply(it) { type, condition, amount ->
         now {
             val player = player().castSafely<Player>() ?: return@now false
+            val inventory = player.inventory
             when (type) {
                 "check", "has" -> {
-                    player.inventory.hasItem(amount.getOrElse { 1 }) { itemStack ->
+                    inventory.hasItem(amount.getOrElse { 1 }) { itemStack ->
+                        Arim.itemMatch.match(itemStack, condition)
+                    }
+                }
+
+                "take" -> {
+                    val has = inventory.hasItem(amount.getOrElse { 1 }) { itemStack ->
+                        Arim.itemMatch.match(itemStack, condition)
+                    }
+                    inventory.takeItem(amount.getOrElse { 1 }) { itemStack ->
+                        Arim.itemMatch.match(itemStack, condition)
+                    }
+                    has
+                }
+
+                "count" -> {
+                    inventory.countItem { itemStack ->
                         Arim.itemMatch.match(itemStack, condition)
                     }
                 }
